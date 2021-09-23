fa727f5054ce1f86c539278edf32457c7d0e9b04
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2021-09-23 08:55:13 -0400

    Change Reports

--- app/core/reports/urls.py
+++ app/core/reports/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # REPORTES BASCULA    
     path('rpt_bascula001/', RptBascula001ReportView.as_view(), name='rpt_bascula001'),
     path('rpt_bascula002/', RptBascula002ReportView.as_view(), name='rpt_bascula002'),
+    path('rpt_bascula003/', RptBascula003ReportView.as_view(), name='rpt_bascula003'),
     path('rpt_bascula004/', RptBascula004ReportView.as_view(), name='rpt_bascula004'),
     path('rpt_bascula005/', RptBascula005ReportView.as_view(), name='rpt_bascula005'),
 
