1dfff263afa3a868484c7cb2509fb87073eb08a0
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2021-11-13 13:24:32 -0300

    Change Movimientos Admin

--- app/core/bascula/admin.py
+++ app/core/bascula/admin.py
@@ -3,28 +3,32 @@ from core.base.admin import ModeloAdminBase
 from core.bascula.models import *
 
 # Register your models here.
+
+
 class ConfigSerialModelAdmin(ModeloAdminBase):
-    list_display = ('cod','__str__','descripcion',)
+    list_display = ('cod', '__str__', 'descripcion',)
 
 class MonedaAdmin(ModeloAdminBase):
-    list_display = ('cod_moneda','denominacion','precio_compra','precio_venta')
+    list_display = ('cod_moneda', 'denominacion',
+                    'precio_compra', 'precio_venta')
     search_fields = ('denominacion',)
 
 class MovimientoAdmin(ModeloAdminBase):
-    list_display = ('id','fecha','nro_ticket','cliente','producto','vehiculo','chofer','fec_impresion')
-    search_fields = ['id','cliente__denominacion','producto__denominacion','chofer__nombre',
-                     'chofer__apellido','nro_ticket','vehiculo__matricula']
-    readonly_fields = ('fecha', 'nro_ticket','fec_entrada', 'fec_salida',
+    list_display = ('id', 'fecha', 'nro_ticket', 'cliente',
+                    'producto', 'vehiculo', 'chofer', 'fec_impresion')
+    search_fields = ['id', 'cliente__denominacion', 'producto__denominacion', 'chofer__nombre',
+                     'chofer__apellido', 'nro_ticket', 'vehiculo__matricula']
+    readonly_fields = ('fecha', 'nro_ticket', 'fec_entrada', 'fec_salida',
                        'usu_insercion', 'fec_insercion', 'usu_modificacion', 'fec_modificacion',)
-    list_editable =['fec_impresion'] #Consume muchos recursos (tarda mucho la consulta)
-    list_filter =['cliente','producto','vehiculo','chofer','fecha']
+    # Consume muchos recursos (tarda mucho la consulta)
+    list_editable = ['fec_impresion']
+    list_filter = ['cliente', 'producto', 'vehiculo', 'chofer', 'fecha']
     list_per_page = 10
 
 # REGISTRO DE MODELOS
-admin.site.register(ConfigSerial,ConfigSerialModelAdmin)
-admin.site.register(MarcaVehiculo,ModeloAdminBase)
-admin.site.register(Cliente,ModeloAdminBase)
-admin.site.register(Categoria,ModeloAdminBase)
-admin.site.register(Producto,ModeloAdminBase)
-admin.site.register(Movimiento,MovimientoAdmin)
-
+admin.site.register(ConfigSerial, ConfigSerialModelAdmin)
+admin.site.register(MarcaVehiculo, ModeloAdminBase)
+admin.site.register(Cliente, ModeloAdminBase)
+admin.site.register(Categoria, ModeloAdminBase)
+admin.site.register(Producto, ModeloAdminBase)
+admin.site.register(Movimiento, MovimientoAdmin)
