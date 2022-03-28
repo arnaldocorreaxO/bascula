8313758b563a57302778f3cec8fe81c361e43276
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2022-03-28 08:09:22 -0400

    Bascula V2.0

--- app/core/bascula/static/vehiculo/js/list.js
+++ app/core/bascula/static/vehiculo/js/list.js
@@ -28,10 +28,10 @@ var vehiculo = {
             order: [[0, 'asc']],
             columns: [
                 {"data": "id"},
-                {"data": "fecha"},
-                {"data": "vehiculo"},
-                {"data": "chofer"},
-                {"data": "peso_neto"},
+                {"data": "matricula"},
+                {"data": "marca"},
+                {"data": "transporte"},
+                {"data": "id"},
             ],
             columnDefs: [
                 {
