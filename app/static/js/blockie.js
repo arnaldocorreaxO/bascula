f7a32ebc0365c988e53db61403198a72d5ab8aa4
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2023-06-22 09:09:56 -0400

    Fix security browser

---
+++ app/static/js/blockie.js
@@ -0,0 +1,43 @@
+
+//Validar Navegador Chrome
+function isChrome(){
+    if(/chrom(e|ium)/.test(navigator.userAgent.toLowerCase())){
+    //   alert("estas en chrome");
+      return True;
+    }else{
+        $('body').html('Esta web solo puede ser utilizada con el navegador <a href="https://www.google.com/chrome/">Google Chrome</a>');  
+    }
+   };
+
+// Bloquear inspeccionar elemento
+function blockie() {
+    // document.addEventListener("contextmenu", (e) => {
+    //     e.preventDefault();
+    // });
+    window.addEventListener("keydown", (e) => {
+        // e.preventDefault();
+        // console.log(e);
+        let handled = false;
+        if (e.code == "F12") {
+            // Handle the event with KeyboardEvent.key
+            handled = true;
+        }
+        if (e.ctrlKey==true & e.shiftKey ==true & e.code == "KeyI") {
+            // Handle the event with KeyboardEvent.key
+            handled = true;
+        }
+        if (e.ctrlKey==true & e.code == "KeyU") {
+            // Handle the event with KeyboardEvent.key
+            handled = true;
+        }
+        if (handled) {
+            // Suppress "double action" if event handled
+            e.preventDefault();
+        }
+    },
+        true
+    )
+
+}
+
+
